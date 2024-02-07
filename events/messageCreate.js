e65cc0be5e95bccc1c3036d33ffaf0a35b5a8c38
UnKnownn <pandaepic01@gmail.com>
2024-02-07 16:46:26 +0530

    added ++relic too

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Client, Message } = require('discord.js')
+const { Client, Message, EmbedBuilder } = require('discord.js')
 const { success, err } = require('../data/utils');
 const prefix = require('../configs/config.json').prefix
 const fs = require('node:fs')
@@ -19,40 +19,57 @@ module.exports = {
         try {
             word = message.content.split(' ')[0].slice(2)
             const command = client.treasury.get(word.toLocaleLowerCase())
-            if (command.data) {
+            if (command?.data) {
                 const reply = await message.reply({ content: `Unknown command. Perhaps its a slash command?` })
                 setTimeout(() => {
                     reply.delete()
                 }, 3000);
                 return;
+            } else if (command) {
+                command?.execute(client, message)
             } else {
-                command.execute(client, message)
-            }
-            success(`COMMAND`, `"${word}" by ${message.member.nickname??message.member.user.username}`)
-            
-            // Checking if its something like ++lg1, similar to soup
-            var era, type, howmany, firstindex, currentEra, finalRelic;
+                success(`COMMAND`, `"${word}" by ${message.member.nickname??message.member.user.username}`)
 
-            try {
-                firstindex = word.search(/[a-zA-Z]/)
-            } catch (e) { return; }
-            howmany = word.slice(0, firstindex)
-            currentEra = word.slice(firstindex)
-            if (!howmany) return;
-            else if (!isNaN(word)) return;
-            else if (currentEra[0] === 'a') era = "Axi"
-            else if (currentEra[0] === 'n') era = "Neo"
-            else if (currentEra[0] === 'm') era = "Meso"
-            else if (currentEra[0] === 'l') era = "Lith"
-            type = word.slice(firstindex + 1).toUpperCase()
+                // Checking if its something like ++lg1, similar to soup
+                var relicEra, relicType, relicShort, finalRelic;
 
-            let jsfile = await JSON.parse(await fs.readFileSync('./data/relicdata.json', 'utf-8'))
-            for (const relic of jsfile) {
-                if (relic[0][0] == `${era} ${type}`) finalRelic = relic.map(x => x[0]), relic.map(x => x[1])
+                if (['meso', 'neo', 'axi', 'lith'].includes(word)) {
+                    relicEra = word[0].toLocaleUpperCase() + word.slice(1)
+                    relicType = message.content.split(' ')[1].toLocaleUpperCase() ?? 'a'
+                } else {
+                    relicShort = word.slice(0, 1)
+                    if (!isNaN(word)) return;
+                    else if (relicShort[0] === 'a') relicEra = "Axi"
+                    else if (relicShort[0] === 'n') relicEra = "Neo"
+                    else if (relicShort[0] === 'm') relicEra = "Meso"
+                    else if (relicShort[0] === 'l') relicEra = "Lith"
+                    relicType = word.slice(1).toUpperCase()
+                }
+
+                let jsfile = await JSON.parse(await fs.readFileSync('./data/relicdata.json', 'utf-8'))
+                for (const relic of jsfile) {
+                    if (relic[0][0] == `${relicEra} ${relicType}`) finalRelic = relic.map(x => x[0]), relic.map(x => x[1])
+                }
+
+                if (finalRelic) {
+                    var descstring = ""
+                    for (var n = 0; n < 5; n++) {
+                        var bindex = finalRelic[n+1].indexOf('[') ?? -1
+                        descstring += `${`${finalRelic[n+1].slice(bindex == -1 ? 100 : bindex+1, -1)}`.padEnd(3)} | ${finalRelic[n+1].slice(0, bindex == -1 ? 100 : bindex)}\n`
+                    }
+                    await message.reply({ embeds: [
+                        new EmbedBuilder()
+                        .setTitle(`[ ${finalRelic[0]} ] {${finalRelic[7]}}`)
+                        .setDescription(`
+\`\`\`ml
+${descstring}
+\`\`\`
+`) ] })
+                }
             }
-            console.log(finalRelic)
         } catch (error) {
             err(error, `"${word}" by ${message.member.nickname??message.member.user.username}`)
+            console.log(error)
         }
     },
 };
