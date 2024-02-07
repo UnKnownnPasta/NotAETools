85e4adf311e57755af5d597b86c342639ad3401a
UnKnownn <pandaepic01@gmail.com>
2024-02-07 22:37:52 +0530

    fixes

--- treasury/messagetype/anyrelic.js
+++ treasury/messagetype/anyrelic.js
@@ -1,6 +1,15 @@
 const { Client, Message, codeBlock, EmbedBuilder } = require('discord.js')
 const fs = require('node:fs')
 
+const types = { 
+    "#0d071e": "ED",
+    "#3c0000": "RED",
+    "#472502": "ORANGE",
+    "#4b3800": "YELLOW",
+    "#162e0b": "GREEN",
+    "#282828": undefined
+};
+
 module.exports = {
     name: ['*'],
     /**
@@ -26,19 +35,19 @@ module.exports = {
 
         let jsfile = await JSON.parse(await fs.readFileSync('./data/relicdata.json', 'utf-8'))
         for (const relic of jsfile) {
-            if (relic[0][0] == `${relicEra} ${relicType}`) finalRelic = relic.map(x => x[0]), relic.map(x => x[1])
+            if (relic[0][0] == `${relicEra} ${relicType}`) finalRelic = relic.map(x => [x[0], `${types[x[1]] ? `{${types[x[1]]}}` : ''}`])
         }
 
         const rarities = ['C', 'C', 'C', 'UC', 'UC', 'RA']
         if (finalRelic) {
             var descstring = ""
             for (var n = 0; n < 6; n++) {
-                var bindex = finalRelic[n+1].indexOf('[') ?? -1
-                descstring += `${rarities[n].padEnd(2)} | ${`${finalRelic[n+1].slice(bindex == -1 ? 100 : bindex+1, -1)}`.padEnd(2)} | ${finalRelic[n+1].slice(0, bindex == -1 ? 100 : bindex)}\n`
+                var bindex = finalRelic[n+1][0].indexOf('[') ?? -1
+                descstring += `${rarities[n].padEnd(2)} | ${`${finalRelic[n+1][0].slice(bindex == -1 ? 100 : bindex+1, -1)}`.padEnd(2)} | ${finalRelic[n+1][0].slice(0, bindex == -1 ? 100 : bindex)}${finalRelic[n+1][1]}\n`
             }
             await message.reply({ embeds: [
                 new EmbedBuilder()
-                .setTitle(`[ ${finalRelic[0]} ] {${finalRelic[7]}}`)
+                .setTitle(`[ ${finalRelic[0][0]} ] {${finalRelic[7][0]}}`)
                 .setDescription(`${codeBlock('ml', descstring)}`) ] })
         }
     }
