9c12990a9d239cedb9b23c68cffa6b9f258945db
UnKnownn <pandaepic01@gmail.com>
2024-02-09 19:12:46 +0530

    QOL + features 3/3

--- data/utils.js
+++ data/utils.js
@@ -68,11 +68,25 @@ async function updateFissures(client) {
   await client.channels.cache.get(fissureChannel).messages.fetch({ limit: 1 }).then(async (msg) => await msg.first().edit({ embeds: [NormEmbed, SPEmbed] }))
 }
 
+function titleCase(str) {
+  const words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'bp') {
+      words[i] = 'BP';
+      continue;
+    }
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+  }
+  return words.join(' ');
+}
+
 module.exports = {
   err,
   alert,
   checkForRelic,
-  updateFissures
+  updateFissures,
+  titleCase
 }
 
 /* Fissures are like
--- treasury/messagetype/anyrelic.js
+++ treasury/messagetype/anyrelic.js
@@ -1,6 +1,6 @@
 const { Client, Message, codeBlock, EmbedBuilder } = require('discord.js')
 const fs = require('node:fs')
-const { checkForRelic } = require('../../data/utils')
+const { checkForRelic, titleCase } = require('../../data/utils')
 
 const types = { 
     "#150b2e": "ED",
@@ -19,27 +19,56 @@ module.exports = {
     * @param {Message} message
     */
     async execute(client, message, word) {
-        var finalRelic;
+        var finalRelic = [];
         var verifiedRelic = checkForRelic(word)
         if (!verifiedRelic) return;
 
+        let detectedType = ""
+        const rarities = ['C', 'C', 'C', 'UC', 'UC', 'RA']
         let jsfile = await JSON.parse(await fs.readFileSync('./data/relicdata.json', 'utf-8'))
+
         for (const relic of jsfile) {
-            if (relic[0][0] == verifiedRelic) finalRelic = relic.map(x => [x[0], `${types[x[1]] ? `{${types[x[1]]}}` : ''}`])
+            if (relic[0][0] == verifiedRelic) {
+                finalRelic = relic.map(x => [x[0], `${types[x[1]] ? `{${types[x[1]]}}` : ''}`])
+                detectedType = "relic"
+            } else if (relic.map(x => x[0].slice(0, x[0].indexOf('[')-1)).includes(titleCase(message.content.slice(2)))) {
+                const ind = relic.map(x => x[0].slice(0, x[0].indexOf('[')-1)).indexOf(titleCase(message.content.slice(2)))
+                finalRelic.push([rarities[ind - 1], `${relic[0][0]} {${relic[7][0]}}`])
+                detectedType = 'part'
+            }
+        }
+        console.log(finalRelic)
+
+        switch (detectedType) {
+            case 'relic':
+                if (finalRelic) {
+                    var descstring = ""
+                    for (var n = 1; n < 7; n++) {
+                        var bindex = finalRelic[n][0].indexOf('[') ?? -1
+                        descstring += `${rarities[n-1].padEnd(2)} | ${`${finalRelic[n][0].slice(bindex == -1 ? 100 : bindex+1, -1)}`.padEnd(2)} | ${finalRelic[n][0].slice(0, bindex == -1 ? 100 : bindex)}${finalRelic[n][1]}\n`
+                    }
+                    await message.reply({ embeds: [
+                        new EmbedBuilder()
+                        .setTitle(`[ ${finalRelic[0][0]} ] {${finalRelic[7][0]}}`)
+                        .setDescription(`${codeBlock('ml', descstring)}`) 
+                    ] })
+                }
+            break;
+            case 'part':
+                if (finalRelic) {
+                    var descstring = ""
+                    for (const part of finalRelic) {
+                        descstring += `${`${part[0]}`.padEnd(2)} | ${part[1]}\n`
+                    }
+                    await message.reply({ embeds: [
+                        new EmbedBuilder()
+                        .setTitle(`[ ${titleCase(message.content.slice(2))} ] Relics`)
+                        .setDescription(`${codeBlock('ml', descstring)}`) 
+                    ] })
+                }
+            break;
         }
 
-        const rarities = ['C', 'C', 'C', 'UC', 'UC', 'RA']
-        if (finalRelic) {
-            var descstring = ""
-            for (var n = 1; n < 7; n++) {
-                var bindex = finalRelic[n][0].indexOf('[') ?? -1
-                descstring += `${rarities[n-1].padEnd(2)} | ${`${finalRelic[n][0].slice(bindex == -1 ? 100 : bindex+1, -1)}`.padEnd(2)} | ${finalRelic[n][0].slice(0, bindex == -1 ? 100 : bindex)}${finalRelic[n][1]}\n`
-            }
-            await message.reply({ embeds: [
-                new EmbedBuilder()
-                .setTitle(`[ ${finalRelic[0][0]} ] {${finalRelic[7][0]}}`)
-                .setDescription(`${codeBlock('ml', descstring)}`) 
-            ] })
-        }
+        
     }
 }
