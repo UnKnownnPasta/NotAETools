355fcd0c5925e241d7b308aaee9c80f656f10989
UnKnownn <pandaepic01@gmail.com>
2024-02-21 18:02:15 +0530

    small tinkering

--- events/listeners/msgCreate.js
+++ events/listeners/msgCreate.js
@@ -1,40 +1,50 @@
-const { Message, Client } = require('discord.js')
-const config = require('../../data/config.json')
-const { filterRelic, relicExists, titleCase } = require('../../data/scripts/utility')
+const { Message, Client } = require("discord.js");
+const config = require("../../data/config.json");
+const {
+    filterRelic,
+    relicExists,
+    titleCase,
+    info,
+} = require("../../data/scripts/utility");
 
 module.exports = {
-    name: 'messageCreate',
+    name: "messageCreate",
     once: false,
     /**
      * messageCreate event listener
-     * @param {Client} client 
-     * @param {Message} message 
+     * @param {Client} client
+     * @param {Message} message
      */
     async listen(client, message) {
-        if (!message.content.startsWith(config.prefix) || message.author.bot) 
+        if (!message.content.startsWith(config.prefix) || message.author.bot)
             return;
 
-        let word = message.content.slice(2).toLocaleLowerCase()
+        let word = message.content.slice(2).toLocaleLowerCase();
         let cmdType = "";
 
-        let isPrime = word.split(' ').includes('prime')
-        let isRelic = await relicExists(filterRelic(word))
-        let isStatus = ['ed', 'red', 'orange', 'green'].some(x => word.indexOf(x) != -1)
-        
+        let isPrime = word.split(" ").includes("prime");
+        let isRelic = await relicExists(filterRelic(word));
+        let isStatus = ["ed", "red", "orange", "green"].some(
+            (x) => word.indexOf(x) != -1
+        );
+
         // 1st check: not relic not prime and is ed
         // 2nd check: not relic not ed not prime
         // 3rd check: not relic not ed and is prime
         // 4th check: not prime not ed and is relic
         if (!isRelic && isStatus && !isPrime) {
-            cmdType = 'status'
+            cmdType = "status";
         } else if (!isRelic && !isStatus && !isPrime) {
-            cmdType = 'part'
+            cmdType = "part";
         } else if (!isRelic && !isStatus && isPrime) {
-            cmdType = "prime"
+            cmdType = "prime";
         } else if (isRelic && !isStatus && !isPrime) {
-            cmdType = "relic"
+            cmdType = "relic";
         }
 
-        client.treasury.get('anycmd').execute(client, message, titleCase(word), cmdType)
-    }
-}
+        info("CMD", `Ran ++ command by ${message.member.nickname ?? message.author.username} with arguments: "${word}"`);
+        client.treasury
+            .get("anycmd")
+            .execute(client, message, titleCase(word), cmdType);
+    },
+};
--- treasury/anycmd.js
+++ treasury/anycmd.js
@@ -1,111 +1,134 @@
 // const { spreadsheet } = require('../data/config.json')
-const { EmbedBuilder, codeBlock, ButtonStyle } = require('discord.js');
-const fs = require('node:fs')
-const { Pagination } = require('pagination.djs');
-const { filterRelic } = require('../data/scripts/utility.js');
+const { EmbedBuilder, codeBlock, ButtonStyle } = require("discord.js");
+const fs = require("node:fs");
+const { Pagination } = require("pagination.djs");
+const { filterRelic } = require("../data/scripts/utility.js");
 
 module.exports = {
-    name: 'anycmd',
+    name: "anycmd",
     async execute(client, message, wd, type) {
-        const allrelics = (await JSON.parse(fs.readFileSync('./data/relicdata.json')))
-        const word = wd.replace(/--[r]/, '').trim()
+        const allrelics = await JSON.parse(
+            fs.readFileSync("./data/relicdata.json")
+        );
+        const word = wd.replace(/--[r]/, "").trim();
 
         switch (type) {
-            case 'status':
-                let edlist = []
-                allrelics.relicData.forEach(part => {
-                    part.slice(1, 7).forEach(p => {
-                        if (p.type == word.toUpperCase()) edlist.push(`${`[${p.count}]`.padEnd(3)} | ${p.name}`);
-                    })
+            case "status":
+                let edlist = [];
+                allrelics.relicData.forEach((part) => {
+                    part.slice(1, 7).forEach((p) => {
+                        if (p.type === word.toUpperCase())
+                            edlist.push(`${`[${p.count}]`.padEnd(3)} | ${p.name}`);
+                    });
                 });
 
-                edlist = [... new Set(edlist)].sort((a, b) => a.split('|')[0].match(/\[(.+?)\]/)[1] - b.split('|')[0].match(/\[(.+?)\]/)[1])
-                const embedOfParts = []
-                for (let i=0; i < edlist.length; i+=15) {
+                edlist = [...new Set(edlist)].sort(
+                    (a, b) =>
+                        a.split("|")[0].match(/\[(.+?)\]/)[1] -
+                        b.split("|")[0].match(/\[(.+?)\]/)[1]
+                );
+                const embedOfParts = [];
+                for (let i = 0; i < edlist.length; i += 15) {
                     embedOfParts.push(
-                        new EmbedBuilder().setTitle(`[ ${word.toUpperCase()} ]`)
-                        .setDescription(codeBlock('ml', edlist.slice(i, i+15).join('\n')))
-                    )
+                        new EmbedBuilder()
+                            .setTitle(`[ ${word.toUpperCase()} ]`)
+                            .setDescription(codeBlock("ml", edlist.slice(i, i + 15).join("\n")))
+                    );
                 }
 
                 const pagination = new Pagination(message, {
-                    firstEmoji: '⏮',
-                    prevEmoji: '◀️',
-                    nextEmoji: '▶️',
-                    lastEmoji: '⏭',
-                    idle: 60000, 
-                    buttonStyle: ButtonStyle.Secondary, 
-                    loop: true 
+                    firstEmoji: "⏮",
+                    prevEmoji: "◀️",
+                    nextEmoji: "▶️",
+                    lastEmoji: "⏭",
+                    idle: 60000,
+                    buttonStyle: ButtonStyle.Secondary,
+                    loop: true,
                 });
-        
+
                 pagination.setEmbeds(embedOfParts, (embed, index, array) => {
-                    return embed.setFooter({ text: `Page ${index + 1}/${array.length}` });
+                    return embed.setFooter({
+                        text: `Page ${index + 1}/${array.length}`,
+                    });
                 });
                 pagination.render();
-            break;
-            
-            case 'part':
+                break;
+
+            case "part":
                 if (!allrelics.partNames.includes(word)) return;
-                const scarcity = ['C', 'C', 'C', 'UC', 'UC', 'RA']
+                const scarcity = ["C", "C", "C", "UC", "UC", "RA"];
                 let countOfPart;
 
-                const relicList = allrelics.relicData.map(relic => {
-                    if (relic[0].has.some(n => n.indexOf(word) !== -1)) {
-                        const item = relic[0].has.findIndex(x => x.indexOf(word) !== -1) + 1
-                        if (!countOfPart) countOfPart = x[item].count
-                        return `${scarcity[item-1].padEnd(2)} | ${relic[0].name} {${relic[0].tokens}}`
-                    }
-                }).filter(x => x!=undefined)
-                
-                await message.reply({ embeds: [
-                    new EmbedBuilder().setTitle(`[ ${word} ] {x${countOfPart}}`)
-                    .setDescription(codeBlock('ml', relicList.join('\n')))
-                ] })
-            break;
-
-            case 'prime':
-                const corrWord = word.replace('Prime', '').trim()
-                let parts = []
-
-                let getAllRelics = allrelics.relicData.map(relic => {
-                    let foundAny = false
-                    relic[0].has.forEach((part, i) => {
-                        if (part.indexOf(corrWord) !== -1) {
-                            parts.push(relic[i+1])
-                            foundAny = true
-                        }
+                const relicList = allrelics.relicData
+                    .map((relic) => {
+                        const item = relic[0].has.findIndex((x) => x.indexOf(word) !== -1);
+                        if (item === -1) return;
+                        if (!countOfPart) countOfPart = relic[item + 1].count;
+                        return `${scarcity[item].padEnd(2)} | ${relic[0].name} {${relic[0].tokens}}`;
                     })
-                    if (foundAny) return relic[0];
-                }).filter(x => x!=undefined)
+                    .filter((x) => x !== undefined);
 
-                parts = parts.map(x => `${x.count.padEnd(2)} | ${x.name}`)
-                parts = [... new Set(parts)]
+                await message.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(`[ ${word} ] {x${countOfPart}}`)
+                            .setDescription(codeBlock("ml", relicList.join("\n"))),
+                    ],
+                });
+                break;
+
+            case "prime":
+                const corrWord = word.replace("Prime", "").trim();
+                let parts = [];
+
+                let getAllRelics = allrelics.relicData
+                    .map((relic) => {
+                        let foundAny = false;
+                        relic[0].has.forEach((part, i) => {
+                            if (part.indexOf(corrWord) !== -1) {
+                                parts.push(relic[i + 1]);
+                                foundAny = true;
+                            }
+                        });
+                        if (foundAny) return relic[0];
+                    })
+                    .filter((x) => x !== undefined);
+
+                parts = parts.map((x) => `${x.count.padEnd(2)} | ${x.name}`);
+                parts = [...new Set(parts)];
 
                 const embds = [
-                    new EmbedBuilder().setTitle(`[ ${word} ]`)
-                    .setDescription(codeBlock('ml', parts.join('\n')))
-                ]
-
-                if (wd.match(/--[r]/, '') !== null) {
-                    embds.push(
-                        new EmbedBuilder()
-                        .setDescription(codeBlock('ml', getAllRelics.map(x => `${x.tokens.padEnd(2)} | ${x.name}`).join('\n')))
-                    )
-                }
-                
-                message.reply({ embeds: [...embds] })
-            break;
-
-            case 'relic':
-                let frelic = allrelics.relicData.filter(x => x[0].name == filterRelic(word.toLowerCase()))[0]
-                const rarties = ['C', 'C', 'C', 'UC', 'UC', 'RA']
-                let emstr = frelic.slice(1, 7).map((x, i) => `${rarties[i].padEnd(2)} | ${x.count.padEnd(2)} | ${x.name} ${x.type == ""? "" : `{${x.type}}`}`)
-                message.reply({ embeds: [
                     new EmbedBuilder()
-                    .setTitle(`[ ${frelic[0].name} ] {${frelic[0].tokens}}`)
-                    .setDescription(codeBlock('ml', emstr.join('\n')))
-                ] })
-            break;
+                        .setTitle(`[ ${word} ]`)
+                        .setDescription(codeBlock("ml", parts.join("\n"))),
+                ];
+
+                if (wd.match(/--[r]/, "") !== null) {
+                    embds.push(
+                        new EmbedBuilder().setDescription(codeBlock("ml", getAllRelics.map((x) => `${x.tokens.padEnd(2)} | ${x.name}`).join("\n")))
+                    );
+                }
+
+                message.reply({ embeds: [...embds] });
+                break;
+
+            case "relic":
+                let frelic = allrelics.relicData.filter(
+                    (x) => x[0].name === filterRelic(word.toLowerCase())
+                )[0];
+
+                const rarties = ["C", "C", "C", "UC", "UC", "RA"];
+                let emstr = frelic
+                    .slice(1, 7)
+                    .map((x, i) => `${rarties[i].padEnd(2)} | ${x.count.padEnd(2)} | ${x.name} ${x.type === "" ? "" : `{${x.type}}`}`);
+                message.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(`[ ${frelic[0].name} ] {${frelic[0].tokens}}`)
+                            .setDescription(codeBlock("ml", emstr.join("\n"))),
+                    ],
+                });
+                break;
         }
-    }
-}
+    },
+};
