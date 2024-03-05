477049e996d4e9b736910c7f5cb029d02d415981
UnKnownn <pandaepic01@gmail.com>
2024-03-05 17:42:50 +0530

    Clean up

--- scripts/utility.js
+++ scripts/utility.js
@@ -132,56 +132,79 @@ function getFissureTimings(fisTimes) {
  * Utility function to cycle and display current fissures
  * @param {Client} client 
  */
-async function refreshFissures(client) { // channel id: 1192962141205045328
+async function refreshFissures(client) {
     try {
-        let channel = client.channels.cache.get(fissureChannel)
-        let messageToEdit = await channel.messages.fetch({ limit: 1 })
-        if (messageToEdit.size == 0 || messageToEdit?.first()?.author.id != client.user.id) {
-            messageToEdit = await channel.send({ content: 'Updating fissures...' })
+        let channel = client.channels.cache.get(fissureChannel);
+        if (!channel) return alert('No fissure channel found, Is the channel ID wrong?')
+        let messageToEdit = await channel.messages.fetch({ limit: 1 });
+        if (
+            messageToEdit.size == 0 ||
+            messageToEdit?.first()?.author.id != client.user.id
+        ) {
+            messageToEdit = await channel.send({ content: "Updating fissures..." });
         } else {
-            messageToEdit = messageToEdit.first()
+            messageToEdit = messageToEdit.first();
         }
-        const missions = ['Extermination', 'Capture', 'Sabotage', 'Rescue']
-        const fisres = (await axios.get("https://api.warframestat.us/pc/fissures")).data
+        const missions = ["Extermination", "Capture", "Sabotage", "Rescue"];
+        const fisres = (await axios.get("https://api.warframestat.us/pc/fissures")).data;
         const response = fisres.filter(
-            (f) => !f["isStorm"] && missions.includes(f["missionType"]) && f['active'] && f['tier'] != 'Requiem'
-          )
-    
-        const fissures = await response.map(fis => [titleCase(fis['tier']), `${fis['missionType']} - ${fis['node']} - Ends <t:${new Date(fis['expiry']).getTime()/1000 | 0}:R>\n`, fis['isHard']])
+            (f) =>
+                !f["isStorm"] &&
+                missions.includes(f["missionType"]) &&
+                f["active"] &&
+                f["tier"] != "Requiem"
+        );
+
+        const fissures = await response.map((fis) => [
+            titleCase(fis["tier"]), 
+            `${fis["missionType"]} - ${fis["node"]} - Ends <t:${(new Date(fis["expiry"]).getTime() / 1000) | 0}:R>\n`,
+            fis["isHard"],
+        ]);
         const [N_Embed, S_Embed] = Object.entries(fissures.reduce((acc, fissure) => {
-          let currentEmbed = fissure[2] ? acc.S_Embed : acc.N_Embed
-      
-          currentEmbed[fissure[0]]
-          ? currentEmbed[fissure[0]].value += fissure[1]
-          : currentEmbed[fissure[0]] = { name: fissure[0], value: fissure[1] }
-      
-          return acc;
-        }, {
-          N_Embed: {},
-          S_Embed: {}
-        }))
-      
+            let currentEmbed = fissure[2] ? acc.S_Embed : acc.N_Embed;
+
+            currentEmbed[fissure[0]]
+            ? (currentEmbed[fissure[0]].value += fissure[1])
+            : (currentEmbed[fissure[0]] = { name: fissure[0], value: fissure[1] });
+
+            return acc;
+            }, 
+            { N_Embed: {}, S_Embed: {} }
+        ));
+
         const NormEmbed = new EmbedBuilder()
-         .setTitle('Normal Fissures')
-         .setColor('#2c2c34')
-         .setFields(Object.values(N_Embed[1]).sort((a, b) => b.name.localeCompare(a.name)));
+            .setTitle("Normal Fissures")
+            .setColor("#2c2c34")
+            .setFields(Object.values(N_Embed[1]).sort((a, b) => b.name.localeCompare(a.name)));
         const SPEmbed = new EmbedBuilder()
-         .setTitle('Steel Path Fissures')
-         .setFields(Object.values(S_Embed[1]).sort((a, b) => b.name.localeCompare(a.name)))
-         .setColor('#2c2c34')
-         .setTimestamp();
-        let timeDesc = "Next Fissure Reset:\n"
+            .setTitle("Steel Path Fissures")
+            .setFields(Object.values(S_Embed[1]).sort((a, b) => b.name.localeCompare(a.name)))
+            .setColor("#2c2c34")
+            .setTimestamp();
+        const TimeEmbed = new EmbedBuilder()
+            .setColor("#b6a57f");
+        let timeDesc = "Next Fissure Reset:\n";
 
-        const fisTimes = getFissureTimings(fisres.filter(x => x['tier'] != 'Requiem').map(x => [x['tier'], new Date(x['expiry']).getTime()/1000 | 0] ))
-        const TimeEmbed = new EmbedBuilder().setColor('#b6a57f');
+        const fisTimes = getFissureTimings(
+            fisres
+                .filter((x) => x["tier"] != "Requiem" && x["active"])
+                .map((x) => [
+                    x["tier"],
+                    (new Date(x["expiry"]).getTime() / 1000) | 0,
+                ])
+        );
+
         Array.from(fisTimes.entries()).forEach(([key, val], index) => {
-            timeDesc += `\`${titleCase(key).padEnd(4)}\` <t:${val}:R>\n`
-        })
-        TimeEmbed.setDescription(timeDesc)
+            timeDesc += `\`${titleCase(key).padEnd(4)}\` <t:${val}:R>\n`;
+        });
+        TimeEmbed.setDescription(timeDesc);
 
-        await messageToEdit.edit({ content: null, embeds: [NormEmbed, SPEmbed, TimeEmbed] })
+        await messageToEdit.edit({
+            content: null,
+            embeds: [NormEmbed, SPEmbed, TimeEmbed],
+        });
     } catch (error) {
-        warn('INTRLV', 'Failed to refresh fissures', error)
+        warn("INTRLV", "Failed to refresh fissures", error);
     }
 }
 
