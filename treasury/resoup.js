0d9a3982c3394bbf16740af4fa1facc3f4db194a
UnKnownn <pandaepic01@gmail.com>
2024-05-06 23:43:44 +0530

    Update resoup with new features

--- treasury/resoup.js
+++ treasury/resoup.js
@@ -38,37 +38,50 @@ module.exports = {
         let soupedText = i.options.getString('relics', true),
         filtertype = i.options.getString('filtertype', false) ?? false;
 
-        if (soupedText.match(/[l|m|n|a]\w+\d+/g)) {
+        if (soupedText.match(/\b\d+(l|m|n|a)\w+\d+\b/g)) {
             return i.reply({ content: `Making soup using shorthand like **6lg1** is done using the \`/soup\` command, not \`/resoup\``,
                 ephemeral: true
              })
         }
+
+        const isSpecialMode = soupedText.includes('[0m');
 
         /**
          * @param {String} str 
          * @returns Array
          */
         function parseStringToList(str) {
-            const regex = /\d+x\s*\|\s*[^\|]+?\s*\|\s*\d+\s*ED\s*\|\s*\d+\s*RED\s*\|\s*\d+\s*ORANGE/g;
-            const matches = str.replace(/\{\d+\}\s*\|\s*/g, '').matchAll(regex);
+            let regex;
+            if (isSpecialMode) {
+                str = str.replace(//g, '')
+                regex = /\d+x\s+\[0m\| \[2;34m(Lith|Meso|Neo|Axi) [A-Z]\d+/g;
+            } else {
+                regex = /\b\d+x\s+\|\s+(Lith|Meso|Neo|Axi)\s[A-Z]\d+\b/g;
+            }
+            const matches = str.matchAll(regex);
             return matches || [];
         }
-        const toShort = (txt) => `${txt[0].toLowerCase()}${txt.split(' ')[1].toLowerCase()}`
+        const toShort = (txt) => `${txt[0][0].toLowerCase()}${txt[1].toLowerCase()}`
 
         const filteredToSoups = parseStringToList(soupedText)
 
         const relics = [];
         for (const match of filteredToSoups) {
-            const souptext = match[0].split('|')
-            const index = match[0].indexOf('{') == -1 ? 0 : 1
-            relics.push(`${souptext[index].trim().replace('x', '')}${toShort(souptext[index+1].trim())}`)
+            if (isSpecialMode) {
+                const pmatch = match[0].replace(/\s+\[0m\| /g, ' ').replace('[2;34m', '')
+                const pSplitted = pmatch.replace('|', '').split(/\s+/g)
+                relics.push(`${pSplitted[0].slice(0, -1)}${toShort(pSplitted.slice(1))}`)
+            } else {
+                const splitted = match[0].replace('|', '').split(/\s+/g)
+                relics.push(`${splitted[0].slice(0, -1)}${toShort(splitted.slice(1))}`)
+            }
         }
 
         const [relicsList, boxlist] = await Promise.all([
             JSON.parse(await fs.readFile(path.join(__dirname, '..', 'data', 'RelicData.json'), 'utf-8')),
             JSON.parse(await fs.readFile(path.join(__dirname, '..', 'data', 'BoxData.json'), 'utf-8'))
         ])
-
+        
         const range = (num) => {
             return num >= 0 && num <= 7 ? 'ED'
                 : num > 7 && num <= 15 ? 'RED'
@@ -77,20 +90,26 @@ module.exports = {
                 : 'GREEN'
         }
 
+        const ansiValues = {"ED": "[2;35m", "RED": "[2;31m", "ORANGE": "[2;33m"}
+
         const priorityOfStatus = {"ED": 0, "RED": 1, "ORANGE": 2, "YELLOW": 3, "GREEN": 4, "#N/A": 5}
 
         async function getRelic(name, type=null) {
             for (const relic of relicsList.relicData) {
                 if (relic.name == name) {
+                    const stuffToSpecial = []
                     let StatusArr = relic.rewards.map(part => {
                         let returnrange = range(parseInt(part.stock));
                         if (filtertype == "box") {
                             returnrange = range((boxlist[part.item] ?? 0) + (parseInt(part.stock)))
                         }
+                        if (isSpecialMode) {
+                            stuffToSpecial.push([part.item, priorityOfStatus[returnrange], ansiValues[part.color]])
+                        }
                         return [returnrange, priorityOfStatus[returnrange]];
                     });
                     if (type && type !== "box" && !(Math.min(...StatusArr.map(x => x[1])) <= priorityOfStatus[type.toUpperCase()])) return null;
-                    return [relic.tokens, StatusArr.map(x => x[0])];
+                    return [relic.tokens, StatusArr.map(x => x[0]), ...stuffToSpecial];
                 }
             }
             return null
@@ -121,31 +140,61 @@ module.exports = {
                 }
                 if (isNaN(howmany)) continue;
                 howmany = `${(parseInt(howmany)/3 | 0)*3}`
-
                 soupedAccepted.push(short)
-                const _ = (rarity) => {
-                    return `| ${res[1].filter(x => x == rarity).length}`.padEnd(4) + rarity
+
+                if (isSpecialMode) {
+                    let goodParts = res.slice(2).sort((a, b) => a[1] - b[1]).filter(x => x[2]).slice(0, 2).map(x => {
+                        return `${x[2]}${x[0].replace(" x2", "")}[0m`
+                    })
+
+                    soupedStrings.push(`[2;37m${`{${res[0]}}`.padEnd(5)}[0m| [2;37m${(howmany+'x').padEnd(4)}[0m| [2;34m${rFullName.padEnd(8)}[0m | ${goodParts.join(', ')}`)
+                } else {
+                    const _ = (rarity) => {
+                        return `| ${res[1].filter(x => x == rarity).length}`.padEnd(4) + rarity
+                    }
+                    soupedStrings.push(`${`{${res[0]}}`.padEnd(5)}| ${(howmany+'x').padEnd(4)}| ${rFullName.padEnd(8)} ${_('ED')} ${_('RED')} ${_('ORANGE')}`)
                 }
-                if (res) soupedStrings.push(`${`{${res[0]}}`.padEnd(5)}| ${(howmany+'x').padEnd(4)}| ${rFullName.padEnd(8)} ${_('ED')} ${_('RED')} ${_('ORANGE')}`)
             }
             return soupedStrings
         }
 
         const splitFunc = (r) => {
             const rSplit = r.split('|')
-            const l = rSplit.map(x => x.trim()) // 3, 4, 5
-            const ED = l[3].split()[0]; const RED = l[4].split()[0]; const ORG = l[5].split()[0]
-            return `${ED}${RED}${ORG}-${rSplit[0].match(/\d+/)[0]}-${rSplit[1].match(/\d+/)[0]}`
+            const l = rSplit.map(x => x.trim())
+            if (isSpecialMode) {
+                const countOf = (x) => (r.match(new RegExp(x, 'g')) || []).length;
+                const tokenParse = parseInt(l[0].match(/\{(\d+)\}/)[1]) ?? 0
+                const counts = [countOf('\\[2;35m'), countOf('\\[2;31m'), countOf('\\[2;33m')]
+
+                return [tokenParse, counts, rSplit[1].match(/\d+/)[0]]
+            } else {
+                const ED = l[3].split()[0]; const RED = l[4].split()[0]; const ORG = l[5].split()[0]
+                return `${ED}${RED}${ORG}-${rSplit[0].match(/\d+/)[0]}-${rSplit[1].match(/\d+/)[0]}`
+            }
         }
         const sortFunction = (a, b) => {
-            r1 = splitFunc(a).split('-')
-            r2 = splitFunc(b).split('-')
-            if (r1[0] === r2[0]) {
-                if (r1[1] === r2[1]) return parseInt(r2[2]) - parseInt(r1[2])
-                return parseInt(r2[1]) - parseInt(r1[1])
+            r1 = splitFunc(a)
+            r2 = splitFunc(b)
+            if (!isSpecialMode) {
+                r1 = r1.split('-')
+                r2 = r2.split('-')
+                if (r1[0] === r2[0]) {
+                    if (r1[1] === r2[1]) return parseInt(r2[2]) - parseInt(r1[2])
+                    return parseInt(r2[1]) - parseInt(r1[1])
+                }
+                return r2[0].localeCompare(r1[0])
+            } else {
+                const testa = r1[1].join("")
+                const testb = r2[1].join("")
+                if (testa === testb) {
+                    if (r1[0] === r2[0]) {
+                        return r2[2] - r1[2]
+                    }
+                    return r2[0] - r1[0];
+                }
+                return testb.localeCompare(testa)
             }
-            return r2[0].localeCompare(r1[0])
-        }
+        };
 
         const finishedSoup = (await soupedRelics(relics));
         const filterRelics = (rname) => {
@@ -162,19 +211,19 @@ module.exports = {
         let codeText =  `*CODE: ${soupedAccepted.join(' ')}*`
         if ((soupedString + codeText).length > 4000) 
             return i.reply({ content: `Souped relics is too big to render.`, ephemeral: true })
-
+        
         const currentTimeStamp = `<t:${new Date().getTime() / 1000 | 0}:f>`
         if (duplicateStrings.length !== 0) {
             i.reply({ content: `Duplicates removed: ${duplicateStrings.join(' ')}`, embeds: [ 
                 new EmbedBuilder()
                 .setTitle('Resouped relics')
-                .setDescription(codeBlock('ml', soupedString) + '\n' + codeText + `\n\n${currentTimeStamp} [\`${currentTimeStamp}\`]`)
+                .setDescription((isSpecialMode ? codeBlock('ansi', soupedString) : codeBlock('ml', soupedString)) + '\n' + codeText + `\n\n${currentTimeStamp} [\`${currentTimeStamp}\`]`)
              ] })
         } else {
             i.reply({ embeds: [ 
                 new EmbedBuilder()
                 .setTitle('Resouped relics')
-                .setDescription(codeBlock('ml', soupedString) + '\n' + codeText + `\n\n${currentTimeStamp} [\`${currentTimeStamp}\`]`)
+                .setDescription((isSpecialMode ? codeBlock('ansi', soupedString) : codeBlock('ml', soupedString)) + '\n' + codeText + `\n\n${currentTimeStamp} [\`${currentTimeStamp}\`]`)
              ] })
         }
     }
