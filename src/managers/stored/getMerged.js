1df20c23f827fae630214962e09855b6cabd1e1c
UnKnownn <pandaepic01@gmail.com>
2025-04-20 16:15:20 +0530

    bug fixes for website

--- src/managers/stored/getMerged.js
+++ src/managers/stored/getMerged.js
@@ -1,18 +1,16 @@
 // Load relicsdb.json, load boxCacheManager's sstored box amounts, and add the box amount to all parts in relicsdb.json
-import fs from 'node:fs';
 import boxCacheManager from '../boxCacheManager.js';
+import relicCacheManager from '../relicCacheManager.js';
 
 export async function getMerged() {
-  const relicData = fs.readFileSync('../../data/relicsdb.json', 'utf-8');
-  const boxData = boxCacheManager.boxCache;
+  const relicObject = structuredClone(relicCacheManager.relicCache);
+  const boxData = structuredClone(boxCacheManager.boxCache);
 
   // Process box data to key: value pairs
   const boxObject = {};
   for (const box of boxData) {
     boxObject[box.item] = box.amount;
   }
-
-  const relicObject = JSON.parse(relicData);
 
   // Add box amounts to relicsdb.json
   for (const relic of relicObject.relics) {
