5e9f94b5e900edc3b4cfd69b05b77b24bb9b8fb7
UnKnownn <pandaepic01@gmail.com>
2025-04-20 18:43:47 +0530

    some changes

--- scripts/server.js
+++ scripts/server.js
@@ -6,6 +6,8 @@ import expressStatusMonitor from 'express-status-monitor';
 import { fetchData } from '../src/services/googleSheets.js';
 import { updateFissures } from './fissures.js';
 import { getMerged } from '../src/managers/stored/getMerged.js'
+import { rateLimit } from 'express-rate-limit';
+import mime from 'mime-types';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,15 +16,38 @@ const PORT = process.env.SERVER_PORT || 8080;
 
 // Initialize express-status-monitor
 app.use(expressStatusMonitor());
-
 // Middleware to parse JSON
 app.use(express.json());
-
 // Middleware to serve static files
 app.use(express.static(__dirname));
 
+
+const baseLimitter = {
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+    handler: (req, res, next, options) => {
+      res.status(options.statusCode).json({ error: 'Rate limit exceeded' });
+      next();
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+};
+
+const blobLimiter = rateLimit({
+    ...baseLimitter,
+    max: 1200,
+});
+const apiLimiter = rateLimit({
+    ...baseLimitter,
+    max: 50,
+});
+const globalLimiter = rateLimit({
+    ...baseLimitter,
+    max: 150,
+});
+
 // Route to force an update
-app.get('/forceupdate', async (req, res) => {
+app.get('/forceupdate', apiLimiter, async (req, res) => {
     const token = decodeURIComponent(req.headers.token);
     if (token === process.env.SUPERTOKEN) {
         try {
@@ -50,7 +75,7 @@ app.get('/heartbeat', (req, res) => {
 
 
 // Serve about.html file
-app.get('/', (req, res) => {
+app.get('/', globalLimiter, (req, res) => {
     const filePath = path.join(__dirname, 'about.html');
     fs.readFile(filePath, (err, data) => {
         if (err) {
@@ -62,21 +87,20 @@ app.get('/', (req, res) => {
 });
 
 // Serve static files from the /blob/ directory
-app.get('/blob/*', (req, res) => {
+app.get('/blob/*', blobLimiter, (req, res) => {
     const filePath = path.join(__dirname, '..', req.path);
     fs.readFile(filePath, (err, data) => {
         if (err) {
             res.status(404).send('404 Not Found');
         } else {
-            const extname = path.extname(filePath);
-            const contentType = extname === '.png' ? 'image/png' : extname === '.jpg' ? 'image/jpeg' : 'text/plain';
+            const contentType = mime.lookup(filePath) || 'application/octet-stream';
             res.status(200).contentType(contentType).send(data);
         }
     });
 });
 
 // Endpoint for fissure updating
-app.get('/fissure', async (request, res) => {
+app.get('/fissure', apiLimiter, async (request, res) => {
     const authHeader = (request.headers["X-Source-Job"] || request.headers["x-source-job"]);
     
     if (!authHeader || authHeader !== process.env.EXPECTED_AUTH_TOKEN) {
@@ -94,7 +118,7 @@ app.get('/fissure', async (request, res) => {
 })
 
 // Website endpoint
-app.get('/explorer', (req, res) => {
+app.get('/explorer', globalLimiter, (req, res) => {
     const filePath = path.join(__dirname, 'explorer.html');
     fs.readFile(filePath, (err, data) => {
         if (err) {
@@ -105,11 +129,11 @@ app.get('/explorer', (req, res) => {
     });
 });
 
-app.get('/api/explorer', async (req, res) => {
+app.get('/api/explorer', apiLimiter, async (req, res) => {
     res.json(await getMerged());
 })
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`[EXPRESS] Server is listening on port \`${PORT}\`!`);
 });
